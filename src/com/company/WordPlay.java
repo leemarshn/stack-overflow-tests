c956acc599c0c9b53e6f8900c50f902bf1d3eb49
Lee Ndegwa <leendegwa@gmail.com>
2020-06-22 21:06:44 +0300

    nitial commit

---
+++ src/com/company/WordPlay.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class WordPlay {
+    public static void main(String[] args) {
+    System.out.println(isVowel('a'));
+  }
+  private static boolean isVowel(char ch) {
+    if (ch == 'a') {
+      return true;
+    }
+    return false;
+  }
+}
